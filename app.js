85a08fd87f49929754bd17b53fde3c876292d572
Sanil <patwasanil39@gmail.com>
2024-07-14 22:07:04 +0530

    wanderLust

---
+++ app.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
+main()
+  .then(() => {
+    console.log("Connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
+
+app.get("/", (req, res) => {
+  res.send("This is home page");
+});
+
+// index route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
+
+// New route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+// show route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+// Create route
+
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing({
+    ...req.body.listing,
+    image: {
+      filename: req.body.listing.image.filename,
+      url: req.body.listing.image.url,
+    },
+  });
+  console.log("Image URL:", newListing.image); // Debugging
+  await newListing.save();
+  res.redirect("/listings");
+
+  //   const newListing = new Listing(req.body.listing);
+  //   await newListing.save();
+  //   res.redirect("/listings");
+});
+
+//Edit route
+
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+// update route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+// Delete route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My new villa",
+//     description: "By the Beach",
+//     price: 1200,
+//     location: "Jabalpur",
+//     country: "India",
+//   });
+//   await sampleListing.save();
+//   console.log("Document was saved");
+//   res.send("Successful testing");
+// });
